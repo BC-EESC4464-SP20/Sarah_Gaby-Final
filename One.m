4d9e85ad4c29366c317e7c7141b97cb7c08cc224
sarahashebir <ashebir@bc.edu>
2020-04-21 15:06:47 -0500

    str2double

--- One.m
+++ One.m
@@ -1,12 +1,15 @@
 filename= 'ad_viz_plotval_data.csv';
-%%pm= (filename);
-pm_new= [pm.SiteID, pm.SITE_LATITUDE, pm.SITE_LONGITUDE, pm.DailyMeanPM2_5Concentration]
-total_pm= pm.DailyMeanPM2_5Concentration
-sitelat= unique((pm.SITE_LATITUDE));
-sitelon= unique(pm.SITE_LONGITUDE);
-siteid= unique(pm.SiteID);
+pm = readtable(filename);
+%pm_new= [pm.SiteID, pm.SITE_LATITUDE, pm.SITE_LONGITUDE, pm.DailyMeanPM2_5Concentration]
+total_pm= str2double(pm.DailyMeanPM2_5Concentration)
+sitelat= unique(str2double(pm.SITE_LATITUDE));
+sitelon= unique(str2double(pm.SITE_LONGITUDE));
+ID= unique(str2double(pm.SiteID));
+
 %%
+mean(total_pm(1:15))
+
 %y=splitapply(mean, pm.DailyMeanPM2_5Concentration, pm.SiteID)
 %from list of ids, find averages from a different list in corresponding
 %loop over all site IDs, loop to length site ID and find where site ID of
-%original 
+%original
--- Two.m
+++ Two.m
@@ -1,3 +1,18 @@
 %%Getting minority averages for each town
 %%Mapping towns 
 %%Filling map circles with minority averages
+%% Inital Steps
+filename= 'Dem_lat:lon.csv';
+demlatlon = readtable(filename);
+    %for some reason ^^^ that doesn't work --> but if you import the data
+    %directly it works and creates its own variable - confusing?
+demlat = table2array(Demlatlon(:,2));
+demlon = table2array(Demlatlon(:,3));
+
+filename2 = 'CCASF12010CMAP.xlsx'
+help = readtable(filename2);
+
+
+%% Getting Minority Averages for Each Town
+
+%% Pulling over the Latitude and Longitude
